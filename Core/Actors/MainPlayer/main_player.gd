02af1139e55c6425ee06724dc4927226f56bdfb8
Jackie <jlauravelez@gmail.com>
2025-12-13 20:30:31 +0100

    really weird jumping, hitbox disables when you are jumping

--- Core/Actors/MainPlayer/main_player.gd
+++ Core/Actors/MainPlayer/main_player.gd
@@ -6,7 +6,11 @@ var speed = 20
 @onready var health_manager: HealthManager = $HealthManager
 @onready var hitbox: Area2D = $Hitbox
 signal take_dmg
+var jumping = false
 
+const PLAYER_COLLISION_SHAPE = preload("uid://lhbpgu1bpmcw")
+
+var jump_height = 550
 
 func _ready() -> void:
 	health_manager.took_damage.connect(on_take_dmg)
@@ -28,8 +32,33 @@ func _process(_delta: float) -> void:
 
 			path.global_position = path.global_position - Vector2(direction.y,direction.y) * speed
 
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept") and !jumping:
+		jump()
+	
+func jump():
+	delete_collision()
+	jumping = true
+	var tween = get_tree().create_tween()
+
+	await tween.tween_property(self, "position", Vector2(jump_height,-jump_height), 0.3).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUINT).finished
+	
+	var tween2 = get_tree().create_tween()
+	
+	await tween2.tween_property(self, "position", Vector2(0,0), 0.4).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BOUNCE).finished
+	replace_collision()
+	jumping = false
 	
 
+func replace_collision():
+	var col = PLAYER_COLLISION_SHAPE.instantiate()
+	hitbox.add_child(col)
+
+func delete_collision():
+	for child in hitbox.get_children():
+		if child is CollisionShape2D:
+			child.queue_free()
+	
 
 	
 		
