3724ee934642d1b08f2a3d86e4a67cb5c3b44eba
enrico <hairic95@gmail.com>
2025-12-17 11:47:13 +0100

    added player turn animation

--- Core/Actors/Sledge/sledge.gd
+++ Core/Actors/Sledge/sledge.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 @export var dog : CharacterBody2D
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	velocity = dog.velocity
 	move_and_slide()
+
+func change_frame(frame: int) -> void:
+	$Sprite2D.frame = frame
