87825c45fc06a31a9a8df70479e2827691f296bb
Jackie <jlauravelez@gmail.com>
2025-12-17 11:23:58 +0100

    level movement

--- Core/Level/level_1.gd
+++ Core/Level/level_1.gd
@@ -1,14 +1,15 @@
-@tool
 extends Node2D
 @onready var tile_map_layer: TileMapLayer = $GroundTiles
 @onready var obstacle_tiles: TileMapLayer = $ObstacleTiles
 
 @export var pause : bool = false
-@export_tool_button("reset") var reset_tile = reset_tiles
+#@export_tool_button("reset") var reset_tile = reset_tiles
 var speed = 0.5
 var player_position
 
 var starting_tile_position : Vector2i= Vector2i.ZERO
+
+signal update_y(y)
 
 func _ready() -> void:
 	GameState.gameOver.connect(on_game_over)
@@ -22,16 +23,12 @@ func reset_tiles():
 func _process(delta: float) -> void:
 	if !pause:
 		var map_pos = starting_tile_position - Vector2i(0,1)
-		print("map ", map_pos)
-		
-		starting_tile_position = map_pos
-		var next_pos = map_pos  
-		print("next ", next_pos)
-		var converted = tile_map_layer.map_to_local(next_pos)
-		#print("convert ",converted)
-		#var next_pos = tile_map_layer.map_to_local()  * speed
-		#print(next_pos)
+		update_y.emit(map_pos.y)
+		starting_tile_position = map_pos 
+
+		var converted = tile_map_layer.map_to_local(map_pos)
+
 		tile_map_layer.position = converted
-		#obstacle_tiles.position = next_pos
+		obstacle_tiles.position = converted
 		
 		#print(tile_map_layer.position)
--- Core/UI/ui.gd
+++ Core/UI/ui.gd
@@ -3,17 +3,21 @@ extends CanvasLayer
 @onready var main_player: CharacterBody2D = $"../Path2D/PathFollow2D/Dog"
 @onready var health_bar: TextureProgressBar = %HealthBar
 @onready var score_label: Label = $Control/ScoreLabel
+@onready var node_2d: Node2D = $".."
 
 var score = 0
 
 func _ready() -> void:
 	main_player.take_dmg.connect(on_player_take_dmg)
+	node_2d.update_y.connect(set_label_y)
 
 func on_player_take_dmg(dmg):
 	health_bar.value -= dmg
 	if health_bar.value <= 0:
 		GameState.gameOver.emit()
 
-func _process(delta: float) -> void:
-	score += delta * 10
-	score_label.text = str(int(score))
+func set_label_y(y):
+
+	score_label.text = str(int(abs(y)))
+
+	
