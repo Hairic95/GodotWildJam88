b00e0c95ce0df5bf7f8a9b7d1c71f355d8a630df
enrico <hairic95@gmail.com>
2025-12-19 16:52:49 +0100

    added a couple more rocks and avalanche expressions

--- Core/Obstacles/Obstacle.gd
+++ Core/Obstacles/Obstacle.gd
@@ -1,7 +1,19 @@
 extends Area2D
 class_name Obstacle
 
+var rock_texture_2 = preload("res://assets/textures/actual_rock_2.png")
+var rock_texture_3 = preload("res://assets/textures/actual_rock_3.png")
+
 @export var damage : int = 1
 
 enum Materials {Wood, Stone, Metal}
 @export var material_type : Materials
+
+func _ready() -> void:
+	if material_type == Materials.Stone:
+		match(randi()%3):
+			1:
+				$Sprite2D.texture = rock_texture_2
+			2:
+				$Sprite2D.texture = rock_texture_3
+			
