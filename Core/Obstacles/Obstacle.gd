0fad99231707b18f96cac355613627346164de5a
Jackie <jlauravelez@gmail.com>
2025-12-19 17:25:16 +0100

    obstacle

--- Core/Obstacles/Obstacle.gd
+++ Core/Obstacles/Obstacle.gd
@@ -2,6 +2,13 @@ extends Area2D
 class_name Obstacle
 
 @export var damage : int = 1
+@export var random_scale  : Vector2
 
 enum Materials {Wood, Stone, Metal}
 @export var material_type : Materials
+
+
+func _ready() -> void:
+	if random_scale:
+		var random = randf_range(random_scale.x, random_scale.y)
+		scale = Vector2(random, random)
