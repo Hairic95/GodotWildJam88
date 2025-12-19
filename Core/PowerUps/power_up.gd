5a5ccee9232625275d1c1e09296810fdff9f8d3b
Jackie <jlauravelez@gmail.com>
2025-12-19 16:24:35 +0100

    warm power

--- Core/PowerUps/power_up.gd
+++ Core/PowerUps/power_up.gd
@@ -1,6 +1,6 @@
 extends Area2D
 class_name PowerUps
 
-enum PowerUpTypes {None, Speed, Shield, Alcohol}
+enum PowerUpTypes {None, Speed, Shield, Alcohol, WarmClothes}
 @export var power_type : PowerUpTypes
 @export var amount : int
