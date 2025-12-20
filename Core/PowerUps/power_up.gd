69f93b8fcdb6d4116256fd46772efa15602b7fc2
Jackie <jlauravelez@gmail.com>
2025-12-20 18:12:15 +0100

    kill power up over time

--- Core/PowerUps/power_up.gd
+++ Core/PowerUps/power_up.gd
@@ -4,3 +4,10 @@ class_name PowerUps
 enum PowerUpTypes {None, Speed, Shield, Alcohol, WarmClothes}
 @export var power_type : PowerUpTypes
 @export var amount : int
+@onready var timer: Timer = $Timer
+
+func _ready() -> void:
+	timer.timeout.connect(kill)
+
+func kill():
+	call_deferred("queue_free")
