64bb10058e5efd5536c9ea856516b707e0b35248
Jackie <jlauravelez@gmail.com>
2025-12-20 18:13:03 +0100

    tile speed reword

--- Core/Level/level_1.gd
+++ Core/Level/level_1.gd
@@ -20,15 +20,15 @@ var starting_tile_position : Vector2i= Vector2i.ZERO
 signal update_y(y)
 
 var speed_dictionary = {
-	-4: 2.65,
-	-3: 2.6,
-	-2: 2.55,
-	-1: 2.5,
-	0: 2.45,
-	1: 2.4,
-	2: 2.3,
-	3: 2.2,
-	4: 2.1
+	-4: 20,
+	-3: 40,
+	-2: 60,
+	-1: 80,
+	0: 40,
+	1: 120,
+	2: 140,
+	3: 160,
+	4: 180
 }
 var speed_inc_val = speed_dictionary[GameState.player_speed]
 
@@ -45,19 +45,15 @@ func _ready() -> void:
 func on_timer_timeout():
 	for key in speed_dictionary.keys():
 		speed_dictionary[key] = speed_dictionary[key] -0.1
-	
-	var new_val = speed_dictionary[GameState.player_speed]
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "speed_inc_val",speed_dictionary[GameState.player_speed], 2.0).set_ease(Tween.EASE_IN_OUT)
-	
-	
-	print(speed_dictionary)
+	on_change_speed()
 
 func on_change_speed():
-	var tween = get_tree().create_tween()
-	var new_val = speed_dictionary[GameState.player_speed]
-	tween.tween_property(self, "speed_inc_val",new_val, 2.0).set_ease(Tween.EASE_IN_OUT)
+	call_deferred("alter_speed")
 
+func alter_speed():
+	var new_val = speed_dictionary[GameState.player_speed]
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "speed_inc_val",new_val, 2.0).set_ease(Tween.EASE_IN_OUT)
 func on_set_game_state(state: GameState.States):
 	match(state):
 		GameState.States.MainMenu:
@@ -92,7 +88,6 @@ func _input(event: InputEvent) -> void:
 			mega_zoom_out_vec:
 				camera_2d.zoom = normal_zoom_vec
 			
-				
 func on_game_over():
 	FmodServer.set_global_parameter_by_name("Stage",2)
 	if !debug:
@@ -101,44 +96,45 @@ func on_game_over():
 func reset_tiles():
 	tile_map_layer.position = Vector2.ZERO
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	print("speed inc val ",   speed_dictionary[GameState.player_speed])
 	if !pause:
 		var map_pos = starting_tile_position - Vector2i(0,1)
 		update_y.emit(map_pos.y)
 	
 		starting_tile_position = map_pos 
 
-		var converted = tile_map_layer.map_to_local(map_pos)/speed_inc_val
+		var converted = tile_map_layer.map_to_local(map_pos) * delta * speed_inc_val
 
 		if map_pos.y%110 == 0:
-			place_obstacle(map_pos/speed_inc_val)
-		if map_pos.y%500 == 0:
-			place_powerup(map_pos/speed_inc_val)
+			place_obstacle(map_pos)
+		if map_pos.y%100 == 0:
+			place_powerup(map_pos)
 		if map_pos.y%200 == 0:
-			place_trees(map_pos/speed_inc_val)
+			place_trees(map_pos)
 
 		obstacle_tiles.global_position = converted
 
 func place_powerup(map_pos):
-	var center_marker = obstacle_tiles.local_to_map(%Marker2D.global_position) - Vector2i(map_pos)
+	var center_marker = (%Marker2D.global_position) - Vector2i(map_pos)
 	
 	var power_up_arr = power_up_loot_table.item_results
 	
 	var random_x = randi_range(-30,30)
 	if power_up_arr.size() > 0:
 		var proper_power :Item = power_up_arr[0]
-		var power_up_pos = center_marker - Vector2i(random_x,-100)
+		var power_up_pos = center_marker - Vector2i(random_x,-400)
 		power_up_pos = Vector2i(power_up_pos.x, abs(power_up_pos.y))
 		obstacle_tiles.set_cell(power_up_pos, 5,Vector2.ZERO,proper_power.alternative_tile_id)
 		
 func place_obstacle(map_pos):
 	
-	var center_marker = obstacle_tiles.local_to_map(%Marker2D.global_position) - Vector2i(map_pos)
+	var center_marker = (%Marker2D.global_position) - Vector2i(map_pos)
 	
 	var obstacles_arr = obstacle_loot_table.item_results
 	if obstacles_arr.size() > 0:
 		var proper_obstacle :Item = obstacles_arr[0]
-		var obstacle_pos = center_marker - Vector2i(0,-100)
+		var obstacle_pos = center_marker - Vector2i(0,-400)
 		obstacle_pos = Vector2i(obstacle_pos.x, abs(obstacle_pos.y))
 		obstacle_tiles.set_cell(obstacle_pos, 4,Vector2.ZERO,proper_obstacle.alternative_tile_id)
 	else:
@@ -146,7 +142,7 @@ func place_obstacle(map_pos):
 
 func place_trees(map_pos):
 	
-	var center_marker = obstacle_tiles.local_to_map(%Marker2D.global_position) - Vector2i(map_pos)
+	var center_marker = (%Marker2D.global_position) - Vector2i(map_pos)
 	var folliage_arr = folliage_loot_table.item_results
 	
 	var x1 = -50
@@ -155,10 +151,10 @@ func place_trees(map_pos):
 		var proper_folliage1 :Item = folliage_arr[0]
 		var proper_folliage2 :Item = folliage_arr[1]
 		
-		var folliage_pos = center_marker - Vector2i(x1,-200)
+		var folliage_pos = center_marker - Vector2i(x1,-700)
 		folliage_pos = Vector2i(folliage_pos.x, abs(folliage_pos.y))
 		
-		var folliage_pos_2 = center_marker - Vector2i(x2,-200)
+		var folliage_pos_2 = center_marker - Vector2i(x2,-700)
 		folliage_pos_2 = Vector2i(folliage_pos_2.x, abs(folliage_pos_2.y))
 		
 		obstacle_tiles.set_cell(folliage_pos, 16,Vector2.ZERO,proper_folliage1.alternative_tile_id)
