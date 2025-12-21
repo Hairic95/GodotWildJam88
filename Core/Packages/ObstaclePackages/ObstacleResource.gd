82309158d648aed0e87893c8946f410d6ebc54f3
Jackie <jlauravelez@gmail.com>
2025-12-21 02:04:23 +0100

    obstacles

--- Core/Packages/ObstaclePackages/ObstacleResource.gd
+++ Core/Packages/ObstaclePackages/ObstacleResource.gd
@@ -4,6 +4,9 @@ class_name Obstacle
 @export var damage : int = 10
 @export var random_scale  : Vector2 = Vector2(4,5)
 
+enum ObstacleTypes {PineTree, Rock, Fence}
+@export var obstacle_type : ObstacleTypes
+
 enum Materials {Wood, Stone, Metal}
 @export var material_type : Materials
 
@@ -14,9 +17,12 @@ const ACTUAL_ROCK = preload("uid://dstdu8701nytv")
 const ACTUAL_ROCK_2 = preload("uid://csh62bdmixid7")
 const ACTUAL_ROCK_3 = preload("uid://dx0anbuoonb2d")
 
+const FENCE = preload("uid://yy6oll5h0nya")
+
+
 func choose_random_text():
-	match(material_type):
-		Materials.Stone:
+	match(obstacle_type):
+		ObstacleTypes.Rock:
 			match(randi()%3):
 				1:
 					return ACTUAL_ROCK
@@ -24,12 +30,14 @@ func choose_random_text():
 					return ACTUAL_ROCK_2
 				0:
 					return ACTUAL_ROCK_3
-		Materials.Wood:
+		ObstacleTypes.PineTree:
 			match(randi()%2):
 				1:
 					return TREE_1
 				2:
 					return TREE_2
+		ObstacleTypes.Fence:
+			return FENCE
 
 func return_random_scale():
 	if random_scale:
